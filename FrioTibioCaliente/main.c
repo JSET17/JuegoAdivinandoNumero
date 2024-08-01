144dccb7970135943f0e610fc99312d788fe12d6
Juan Sebastián Escobar Tangarife <jsebastian-1996@hotmail.com>
2024-07-31 20:24:05 -0500

    Done

--- FrioTibioCaliente/main.c
+++ FrioTibioCaliente/main.c
@@ -1,7 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+int num, n, numAlto, numBajo;
+
+void game();
 
 int main() {
-    printf("Hello world!\n");
+    game();
     return 0;
+}
+
+
+void game() {
+    srand(time(NULL));
+    num=rand() % 21;
+
+    numAlto = num + 5;
+    numBajo = num - 5;
+
+    do {
+        system("cls");
+        printf("-----Adivina el numero-----\n\n");
+        printf("Cual es: ");
+        scanf("%d", &n);
+
+        if(n == num) {
+            system("cls");
+            printf("\nAdivinaste.\n");
+            printf("El numero es %d\n\n", num);
+            system("pause");
+        }
+
+        if(n < num) {
+            if( n >= numBajo) {
+                system("cls");
+                printf("\nCaliente.\n\n");
+                system("pause");
+            }
+        }
+
+        if(n > num) {
+            if(n <= numBajo) {
+                system("cls");
+                printf("\nCaliente.\n\n");
+                system("pause");
+            }
+        }
+
+        if(n > num && n > numBajo) {
+            system("cls");
+            printf("\nFrio.\n\n");
+            system("pause");
+        }
+
+        if(n < num && n < numBajo) {
+            system("cls");
+            printf("\nFrio.\n\n");
+            system("pause");
+        }
+    } while(n != num);
 }
